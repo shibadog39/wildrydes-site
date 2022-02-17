02cd408dcd07db4962726b0319ef010fcbf33c95
shibadog39 <shibadog1121@gmail.com>
2022-02-17 21:13:33 +0900

    invokeUrl setting

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@ window._config = {
     region: "ap-northeast-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl:
+      "https://dk4b7bgnoh.execute-api.ap-northeast-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
