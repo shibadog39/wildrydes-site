8f3a9d097dac23ae8a242f1cc5acf970d0475668
shibadog39 <shibadog1121@gmail.com>
2022-02-17 20:29:42 +0900

    userPool setting

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-northeast-1_qsgpoyNMt", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "7gi7ubrg9ein4d18267c6tg6tu", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-northeast-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
